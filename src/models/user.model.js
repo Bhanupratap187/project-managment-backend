914eb0549b98073ec6603429ea2f5161e8717555
Bhanu-quibble <bhanupratap.singh@quibbleai.io>
2025-09-13 11:03:43 +0530

    Add JWT authentication methods to user model and update dependencies

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,10 @@
 import mongoose, { Schema } from 'mongoose';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import crypto from 'crypto';
+
+dotenv.config();
 
 const userSchema = new Schema(
   {
@@ -75,6 +80,41 @@ userSchema.pre('save', function (next) {
 
 userSchema.methods.comparePassword = function (userPassword) {
   return bcrypt.compare(userPassword, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      id: this._id,
+      userName: this.userName,
+      email: this.email,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN },
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN },
+  );
+};
+
+userSchema.methods.generateTemporaryToken = function () {
+  const unHashedToken = crypto.randomBytes(32).toString('hex');
+
+  const hashedToken = crypto
+    .createHash('sha256')
+    .update(unHashedToken)
+    .digest('hex');
+
+  const tokenExpiery = Date.now() + 20 * 60 * 1000; // 20 minutes
+
+  return { unHashedToken, hashedToken, tokenExpiery };
 };
 
 const User = mongoose.model('User', userSchema);
